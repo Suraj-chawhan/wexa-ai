ddc02fd2f62ef1692366414bc7ec949dd8ef25ce
Hunny442917 <surajchauan442919@gmail.com>
2025-07-29 15:30:32 +0530

    new

---
+++ highway/backend/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  name: String,
+  dob: String,
+  otp: String,
+  otpExpires: Date,
+});
+
+module.exports = mongoose.model("User", userSchema);
