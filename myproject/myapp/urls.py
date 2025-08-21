941fd09aef743760c4a3b930dab3fa11db70781a
Suraj Chawhan <112802275+Suraj-chawhan@users.noreply.github.com>
2025-08-21 12:32:11 +0530

    Delete myproject directory

--- myproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from myapp import views
-
-urlpatterns = [
-  path('wheel-specification/', views.wheel_specification_api),
-    path('wheel-specification/<int:id>/', views.wheel_specification_api),
-    path('bogie-checksheet/', views.bogie_checksheet_api),
-    path('bogie-checksheet/<int:id>/', views.bogie_checksheet_api),
-]
